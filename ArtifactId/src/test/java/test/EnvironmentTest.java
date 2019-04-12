c8d887b3cc6cb26bd6588149b0ad371ea09692a1
MariiaNepomiashcha <mariya260691nma@gmail.com>
2019-04-12 02:03:15 +0200

    litecart login test

--- ArtifactId/src/test/java/test/EnvironmentTest.java
+++ ArtifactId/src/test/java/test/EnvironmentTest.java
@@ -4,11 +4,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class EnvironmentTest {
 
@@ -22,12 +19,6 @@ public class EnvironmentTest {
         driver.findElement(By.name("q")).sendKeys("Mariia Nepomiashcha Facebook");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
         driver.quit();
-
-
     }
-
-
-
-
 
 }
